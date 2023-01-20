7fa96f64ef498ab2990fb390e691bfbe89b3402b
Arthur <arthurfraim@gmail.com>
2023-01-20 22:15:11 +0800

    setup firebase

---
+++ config/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyDV2E4QMjo52iPj8m6RdCQmF4V2VZvMNSI",
+  authDomain: "next-firebase-f5801.firebaseapp.com",
+  databaseURL:
+    "https://next-firebase-f5801-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "next-firebase-f5801",
+  storageBucket: "next-firebase-f5801.appspot.com",
+  messagingSenderId: "562202283649",
+  appId: "1:562202283649:web:de308f4b69df9ba9610780",
+};
+
+const app = initializeApp(firebaseConfig);
